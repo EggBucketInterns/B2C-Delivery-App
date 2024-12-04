94295fc12da32481b2e8a95184f87925e3de8c8d
shreyashSinha1208 <sinhashreyash110@gmail.com>
2024-12-04 21:29:02 +0530

    Retrofit implementation error fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
