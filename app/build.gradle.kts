26e4ba44db85260a3a8e85a1e8d75ff767690b30
Shree-sudhanva-k <shreesudhanva2004@gmail.com>
2024-12-18 17:33:13 +0530

    updated date in personal information

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,15 +66,12 @@ dependencies {
 
     // Glide dependencies
     implementation("com.github.bumptech.glide:glide:4.15.1")
-
     kapt("com.github.bumptech.glide:compiler:4.15.1")
     implementation ("com.github.bumptech.glide:annotations:4.15.1")
-
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
-
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
